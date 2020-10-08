e89e4ad51e5d80446d3e82c38f671ee9965aac8e
discordsheep <71639879+discordsheep@users.noreply.github.com>
2020-10-08 12:40:57 +0530

    Add files via upload

---
+++ .bash_history
@@ -0,0 +1,6 @@
+enable-pnpm
+pnpm i discord.js
+pnpm i enmap
+pnpm i better-sqlite3
+refresh
+refresh
---
+++ server.js
@@ -0,0 +1,5 @@
+require("./main")
+
+var express = require("express");
+var http = require("http");
+var app = express();
